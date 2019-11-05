ef6daaef836c4793a7fd02ef2eadd98c2dc30302
Ana Armas Romero <54946499+anaarmas@users.noreply.github.com>
2019-11-05 15:44:50 +0000

    refactor to share codeQL setup code

--- src/all-in-one.ts
+++ src/all-in-one.ts
@@ -1,44 +1,24 @@
 import * as core from '@actions/core';
-import * as toolcache from '@actions/tool-cache';
 import * as exec from '@actions/exec';
 import * as io from '@actions/io';
-import * as path from 'path'
-import * as fs from 'fs'
+import * as path from 'path';
+import * as fs from 'fs';
+import * as setuptools from './setup-tools';
 
 async function run() {
   try {
 
     const language = core.getInput('language', { required: true });
 
-    const version = '1.0.0';
-    const codeqlURL = core.getInput('tools', { required: true });
+    core.startGroup('Setup CodeQL tools');
+    const codeqlSetup = await setuptools.setupCodeQL();
+    const buildtools = path.join(codeqlSetup.tools,'lgtm-buildtools');
+    core.endGroup();
 
-    let codeqlFolder = toolcache.find('CodeQL', version);
-    if (codeqlFolder) {
-      core.debug(`CodeQL found in cache ${codeqlFolder}`);
-    } else {
-      const codeqlPath = await toolcache.downloadTool(codeqlURL);
-      const codeqlExtracted = await toolcache.extractZip(codeqlPath);
-      codeqlFolder = await toolcache.cacheDir(codeqlExtracted, 'CodeQL', version);
-    }
-   
-    const codeqlDist = path.join(codeqlFolder, 'odasa');
-    const codeqlTools = path.join(codeqlDist, 'tools');
-    const codeqlOdasa = path.join(codeqlTools, 'odasa');
-    const projectFolder = path.resolve('project');
-    await io.mkdirP(projectFolder);
-    const snapshotFolder = path.join(projectFolder, 'snapshot');
-
-    const licenseURL = core.getInput('license', { required: true });
-    const licensePath = await toolcache.downloadTool(licenseURL);
-    await io.mkdirP(path.join(codeqlDist, 'license'));
-    await io.cp(licensePath, path.join(codeqlDist, 'license', 'license.dat')); 
-
-    const buildtools = path.join(codeqlTools,'lgtm-buildtools');
-    const sourceLocation = path.resolve('.');
+    core.startGroup('Resolve LGTM configuration');
     const out = path.resolve('out');
     await io.mkdirP(out);
-    const lgtmConfig = path.join('out', 'lgtm.effective.yml');
+    const lgtmConfig = path.join(out, 'lgtm.effective.yml');
     fs.writeFileSync(lgtmConfig,"");
     var possibleConfigLocations = [path.resolve('lgtm.yml'), path.resolve('.lgtm.yml')]
     for (let loc of possibleConfigLocations) {
@@ -47,26 +27,16 @@ async function run() {
             break;
         }
     }
+    core.endGroup();
 
-    if (!process.env.SEMMLE_JAVA_HOME) {
-      let platformSuffix;
-      switch (process.platform) {
-        case 'darwin': {
-          platformSuffix = 'osx';
-          break;
-        }
-        case 'win32': {
-          platformSuffix = 'win';
-          break;
-        }
-        default: {
-          platformSuffix = 'linux';
-        }
-      }
+    core.startGroup('Prepare for CodeQL database extraction');
+    const sourceLocation = path.resolve('.');
+    const projectFolder = path.resolve('project');
+    await io.mkdirP(projectFolder);
+    const snapshotFolder = path.join(projectFolder, 'snapshot');
 
-      core.exportVariable("SEMMLE_JAVA_HOME", path.join(codeqlTools,'java-'+platformSuffix));
-    }
-
+    ensureSemmleJavaHome(codeqlSetup.tools);
+    
     await exec.exec('java', ['-jar',
                       path.join(buildtools, 'lgtmbuild.jar'),
                       buildtools,
@@ -74,29 +44,28 @@ async function run() {
                       sourceLocation,
                       language,
                       lgtmConfig]);
+    core.endGroup();
     
-    await exec.exec(codeqlOdasa, 
-                           [ 'addSnapshot', '--project', projectFolder,
+    await exec.exec(codeqlSetup.odasa, [ 'addSnapshot',
+                             '--project', projectFolder,
                              '--name', 'snapshot', '--default-date',
                              '--default-build',
                              '--default-checkout',
                              '--overwrite', 
                              '--source-location', sourceLocation]);
 
-    await exec.exec(codeqlOdasa,
-                            [ 'findGeneratedCode',
+    await exec.exec(codeqlSetup.odasa, [ 'findGeneratedCode',
                               '--prepare',
                               '--project', projectFolder,
                               snapshotFolder]);
 
-    await exec.exec(codeqlOdasa, [ 'buildSnapshot',
+    await exec.exec(codeqlSetup.odasa, [ 'buildSnapshot',
                                    '--fail-early', '--ignore-errors',
                                    '--overwrite',
                                    '--project', projectFolder,
                                    '--snapshot', snapshotFolder]);
 
-    await exec.exec(codeqlOdasa,
-                            [ 'findGeneratedCode',
+    await exec.exec(codeqlSetup.odasa, [ 'findGeneratedCode',
                               '--output', path.join(projectFolder, 'generated_files.txt'),
                               '--project', projectFolder,
                               snapshotFolder])
@@ -107,3 +76,24 @@ async function run() {
 }
 
 run();
+
+function ensureSemmleJavaHome(codeQLTools : string) {
+  if (!process.env.SEMMLE_JAVA_HOME) {
+    let platformSuffix;
+    switch (process.platform) {
+      case 'darwin': {
+        platformSuffix = 'osx';
+        break;
+      }
+      case 'win32': {
+        platformSuffix = 'win';
+        break;
+      }
+      default: {
+        platformSuffix = 'linux';
+      }
+    }
+
+    core.exportVariable("SEMMLE_JAVA_HOME", path.join(codeQLTools,'java-'+platformSuffix));
+  }
+}
--- src/setup-tracer.ts
+++ src/setup-tracer.ts
@@ -1,45 +1,25 @@
 import * as core from '@actions/core';
-import * as toolcache from '@actions/tool-cache';
 import * as exec from '@actions/exec';
 import * as io from '@actions/io';
 import * as path from 'path';
 import * as fs from 'fs';
+import * as setuptools from './setup-tools';
 
 async function run() {
   try {
     const language = 'cpp';
 
-    const version = '1.0.0';
-    const codeqlURL = core.getInput('tools', { required: true });
-    const licenseURL = core.getInput('license', { required: true });
-
     core.startGroup('Setup CodeQL tools');
-    let codeqlFolder = toolcache.find('CodeQL', version);
-    if (codeqlFolder) {
-      core.debug(`CodeQL found in cache ${codeqlFolder}`);
-    } else {
-      const codeqlPath = await toolcache.downloadTool(codeqlURL);
-      const codeqlExtracted = await toolcache.extractZip(codeqlPath);
-      codeqlFolder = await toolcache.cacheDir(codeqlExtracted, 'CodeQL', version);
-    }
+    const codeqlSetup = await setuptools.setupCodeQL();
     core.endGroup();
    
-    const codeqlDist = path.join(codeqlFolder, 'odasa');
-    const codeqlTools = path.join(codeqlDist, 'tools');
-    const codeqlOdasa = path.join(codeqlTools, 'odasa');
+    const compilerSettings = path.join(codeqlSetup.tools, 'c-compiler-settings-' + (process.platform == 'win32' ? 'win' : 'unix'));
     const tracerConf = path.resolve('tracer.conf');
-    const snapshotFolder = path.resolve('project', 'snapshot');
-
-    const licensePath = await toolcache.downloadTool(licenseURL);
-    await io.mkdirP(path.join(codeqlDist, 'license'));
-    await io.cp(licensePath, path.join(codeqlDist, 'license', 'license.dat'));
-
-    const compilerSettings = path.join(codeqlTools, 'c-compiler-settings-' + (process.platform == 'win32' ? 'win' : 'unix'));
     // Generate tracer configuration
     await exec.exec(
        'java', 
        [ '-cp', 
-         path.join(codeqlTools, 'odasa.jar'), 
+         path.join(codeqlSetup.tools, 'odasa.jar'), 
          'com.semmle.util.io.CompilerReplacementConfigParser', 
          compilerSettings,
          tracerConf
@@ -50,19 +30,19 @@ async function run() {
     if (process.platform != 'win32') {
       data = data.replace(new RegExp('\\\\', 'g'), '/');
     }
-    data = data.replace(new RegExp('\\{0\\}', 'g'), codeqlDist);
+    data = data.replace(new RegExp('\\{0\\}', 'g'), codeqlSetup.dist);
     data = data.replace(new RegExp('\\{1\\}', 'g'), path.resolve('build-tracer.log'));
     fs.writeFileSync(tracerConf, data);
 
-    await exec.exec(codeqlOdasa, [ 'createProject', 'project', '--language', language]);
-    await exec.exec(codeqlOdasa, 
+    await exec.exec(codeqlSetup.odasa, [ 'createProject', 'project', '--language', language]);
+    await exec.exec(codeqlSetup.odasa, 
                            [ 'addSnapshot', '--project', 'project', '--name', 'snapshot', '--default-date', 
                              '--build', 'true', '--checkout', 'true', '--overwrite', 
                              '--source-location', path.resolve('.')
                            ]);
 
     if (process.platform == 'darwin') {
-       core.exportVariable('DYLD_INSERT_LIBRARIES', path.join(codeqlTools, 'libtrace.dylib'));
+       core.exportVariable('DYLD_INSERT_LIBRARIES', path.join(codeqlSetup.tools, 'libtrace.dylib'));
        // create parent folder of SEMMLE_COPY_EXECUTABLES_ROOT
        io.mkdirP('/private/tmp/semmle-c-tracer');
        core.exportVariable('SEMMLE_COPY_EXECUTABLES_ROOT', '/private/tmp/semmle-c-tracer/build');
@@ -70,11 +50,13 @@ async function run() {
     } else if (process.platform == 'win32') {
        await exec.exec('powershell', [ 'src\\inject-tracer.ps1' ], {env: {'ODASA_TRACER_CONFIGURATION': tracerConf}});
     } else {
-       core.exportVariable('LD_PRELOAD', path.join(codeqlTools, '${LIB}trace.so'));
+       core.exportVariable('LD_PRELOAD', path.join(codeqlSetup.tools, '${LIB}trace.so'));
     }
+    
     core.exportVariable('ODASA_TRACER_CONFIGURATION', tracerConf);
+    core.exportVariable('SEMMLE_DIST', codeqlSetup.dist);
+    const snapshotFolder = path.resolve('project', 'snapshot');
     core.exportVariable('ODASA_SNAPSHOT', snapshotFolder);
-    core.exportVariable('SEMMLE_DIST', codeqlDist);
     core.exportVariable('SOURCE_ARCHIVE', path.join(snapshotFolder, 'output', 'src_archive'));
     core.exportVariable('TRAP_FOLDER', path.join(snapshotFolder, 'working', 'trap'));
 
