72f522c0a25e26e2ac29b02c90f758533a2985bf
Arthur Baars <arthur@semmle.com>
2019-11-07 22:56:35 +0100

    Preserve SEMMLE_COPY_EXECUTABLES_ROOT, and other critical tracer vars
    It appears SEMMLE_COPY_EXECUTABLES_ROOT needs to be set explicitly
    for tracing to work correctly on OSX even though it is also set in
    the tracer.spec.environment file.

--- src/tracer-env.ts
+++ src/tracer-env.ts
@@ -1,12 +1,21 @@
 import * as core from '@actions/core';
 
 const config = process.env['ODASA_TRACER_CONFIGURATION'];
+
+const CRITICAL_TRACER_VARS = new Set(
+  [ 'SEMMLE_PRELOAD_libtrace',
+  , 'SEMMLE_RUNNER',
+  , 'SEMMLE_COPY_EXECUTABLES_ROOT',
+  , 'SEMMLE_DEPTRACE_SOCKET',
+  , 'SEMMLE_JAVA_TOOL_OPTIONS'
+  ]);
+
 if (config) {
     const info = { spec: config, env: {} };
     for (let entry of Object.entries(process.env)) {
         const key = entry[0];
         const value = entry[1];
-        if (typeof value !== 'undefined' && key.startsWith('CODEQL')) {
+        if (typeof value !== 'undefined' && (key.startsWith('CODEQL') || CRITICAL_TRACER_VARS.has(key))) {
             info.env[key] = value;
         }
     }
@@ -14,3 +23,4 @@ if (config) {
 } else {
     throw new Error('ODASA_TRACER_CONFIGURATION is not defined');
 }
+
